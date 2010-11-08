93092ad980df39bb7100fc5db5689964a6da0fb4
jenkin90 <jenkin90@ef17d506-aae0-06ea-4d23-8d1ec7ecb975>
2010-11-08 09:15:58 +0000

    Board y city y track han dejado de heredar de graph, city y edge. Ahora board debe contener un grafo, cuyos vertices son city y sus edge son roads. Añadido metodos a city para determinar su pertenencia a canada

--- src/board/City.java
+++ src/board/City.java
@@ -4,7 +4,13 @@ import java.util.Set;
 
 import performance.Performance;
 
-public interface City extends Vertex<City>{
+public interface City {
+	
+	public String getName();
+	public void setName(String name);
+	
+	public Boolean getCanada();
+	public void setCanada();
 	
 	public Boolean hasPerfomance();
 	public Performance getPerformance();
@@ -12,6 +18,5 @@ public interface City extends Vertex<City>{
 	public Set<City> maxMovement(Integer jump);
 	public Set<City> exactMovement(Integer jump);
 	
-	
-	
+	public String toString();
 }
