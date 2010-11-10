c38043e2d4e69647419b18678d42ff8b247eb8f7
fragelbreak <fragelbreak@ef17d506-aae0-06ea-4d23-8d1ec7ecb975>
2010-11-10 11:26:10 +0000

    TalentCard y EventCard más desarrolladas.

--- trunk/src/card/EventCard.java
+++ trunk/src/card/EventCard.java
@@ -1,5 +1,20 @@
 package card;
 
 public interface EventCard extends Card{
+	
+	public String getName(); 
+	// Puede servir también como descripción de la carta. A consultar con grupo.
+	
+	public boolean isSinglePlayer(); 
+	// Teniendo en cuenta que Single player significa jugador en solitario!! 
+	// Solo se usan cartas del 17 al 20.
 
+	public int getNumEventCard(); 
+	// Nos devolverá el número de la carta. Importante para el modo solitario.
+	
+	public int currentMonth(); // Necesitamos saber el mes actual. Probablemente se implemente en 
+	// la clase que lleve la historia.
+	
+	
+	
 }
--- trunk/src/card/TalentCard.java
+++ trunk/src/card/TalentCard.java
@@ -1,5 +1,13 @@
 package card;
 
+import board.City;
+
 public interface TalentCard extends Card{
+	
+	public String getName(); // trivial
+	public String getSalary(); // trivial
+	public boolean isTalentRedLighting(); // Esto hay que aclararlo...
+	public boolean isInUnemployment(); // Talento desempleado (los que se dejan en las ciudades)
+	public City getCityOfUnemployment(); // Nos dirá la ciudad en la que está el talento
 
 }
