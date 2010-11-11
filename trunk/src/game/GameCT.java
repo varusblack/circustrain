a503054c943729787ad43b723040ea8e5727a3cb
lordreivaj <lordreivaj@ef17d506-aae0-06ea-4d23-8d1ec7ecb975>
2010-11-11 14:19:00 +0000

    Añadir los archivos rtf en la carpeta docs

--- trunk/src/game/GameCT.java
+++ trunk/src/game/GameCT.java
@@ -2,11 +2,10 @@ package game;
 
 public interface GameCT {
 	
-	public void startGame(); //Aqui estara el algoritmo que preguntara las condiciones del juego
-	public void runGame(); //Aqui se implementara el juego con muchos if.
-	public void finishGame(); //Reglas finales del juego
+	public void startGame(); 
 	
-	public void finishMonthGame(); //Aqui se implementara las acciones que se hacen al final del mes.
+	public void runGame(); 
 	
-	public void addCityBoard(); //Añadir las ciudades que faltan al tablero.
+	public void finishGame(); 
+	
 }
--- trunk/src/test/Test.java
+++ trunk/src/test/Test.java
@@ -2,11 +2,7 @@ package test;
 
 public class Test {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Hacer test de cada elemento.
 
 	}  
 
