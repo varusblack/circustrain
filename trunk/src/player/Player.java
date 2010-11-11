59419d83fc03bd17b1d89eced17c46f22a58cb92
marcetsii <marcetsii@ef17d506-aae0-06ea-4d23-8d1ec7ecb975>
2010-11-11 16:14:31 +0000

    Hechas las plantillas de asiganciones de Player y ActionCard. Añadidas a la interfaz player más metodos necesarios para la implementación. Eliminada la interfaz talent que estaba en game y no sé que pintaba (con permiso de francis) y arreglados los errores que ha provocado esta eliminación. Para todo lo demás "Masterd Card"

--- trunk/src/player/Player.java
+++ trunk/src/player/Player.java
@@ -2,27 +2,36 @@ package player;
 
 import java.util.List;
 import java.util.Map;
+
+import performance.Performance;
 import card.ActionCard;
-import game.Talent;
+import card.TalentCard;
 
 public interface Player {
+	//Boolean Es circoFamoso, lo tengo que tener en cuenta para después.
 	
 	public Integer getMoney();
-	public Integer getPerformance();
-	public Map<Talent,Integer> getTalents ();
-	
-	public boolean addTalent(Talent t);
-	public boolean addMoney(Integer m);
-	public boolean addPerformance(Integer p);
-	
-	public List<ActionCard> getActionCards();
-	public List<ActionCard> discardActionCard(Integer id);
-	
+	public Integer getPerformanceMax();
+	public Map<TalentCard,Integer> getTalents ();
 	public Integer getReputation();
-	public boolean addReputation();
 	public Integer getVictoryPoints();
 	
+	public boolean addTalent(List<TalentCard> t);
+	public boolean addMoney(Integer m);
+	public boolean addPerformance(Integer p);
 	public boolean addVictoryPoints(Integer vp);
+	public boolean addReputation();
+	
+	public List<Performance> getPerfomancesUsed();
+	public List<ActionCard> getActionCards(); 
+	public List<ActionCard> getdiscartpile();
+	public boolean addActionCard(ActionCard ac);
+	public boolean addPerfomanceUsed(Performance p);
+	public boolean discardActionCard(Integer id);
+	
 	public boolean isFirstPlayer();
+	
+	//private Integer PVCalculate(); [pa dentro de la clase]
+	//private List<Talent> addAditionalsTalents(); [pa dentro de la clase] 
 	
 }
