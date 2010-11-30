497c7cd283e50ec27f45dbdd06ab8d74fbaaac32
vikingore@gmail.com <vikingore@gmail.com@ef17d506-aae0-06ea-4d23-8d1ec7ecb975>
2010-11-30 20:26:06 +0000

    Cambiado tipo de TypeTalentCard a String

--- src/performance/PerformanceDemand.java
+++ src/performance/PerformanceDemand.java
@@ -7,7 +7,8 @@ import card.TypeTalentCard;
 
 
 public interface PerformanceDemand extends Performance {
-		public Map<TypeTalentCard,Integer> getTalentPoints(); /*Devuelve un Map con los talentos y los valores asociaados a una carta*/
+		public Map<String,Integer> getTalentPoints(); /*Devuelve un Map con los talentos y los valores asociaados a una carta*/
+		// he cambiado el tipo de typeTalentCard a String, porque como ha cambiado la clase Talent esta tambien tiene que cambiar
 		public Boolean isTwoWeeks();
 		public Integer getBasicPoints();
 }
