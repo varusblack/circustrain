d788188a0762b098fea02a57e73817bfc817fbb9
marcetsii <marcetsii@ef17d506-aae0-06ea-4d23-8d1ec7ecb975>
2010-12-02 14:09:47 +0000

    Terminadas las ActionCards de Movimiento Básico, Movimiento Rápido, y Viaje. Estan listas y testeadas para ser usadas.

--- src/actionCards/FastMoveImpl.java
+++ src/actionCards/FastMoveImpl.java
@@ -1,21 +1,45 @@
 package actionCards;
 
+import java.util.List;
+
+import board.City;
+import game.factory.GameFactory;
+import player.Player;
+import utiles.factoria.CollectionsFactory;
 import card.CardImpl;
 
 public class FastMoveImpl extends CardImpl implements ActionCard {
+	private Integer id;
+	Player player;
 
-	public FastMoveImpl() {
+	public FastMoveImpl(Player p) {
 		super("Fast Move","You can move until 5 cities");
+		id = 4;
+		player = p;
 	}
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
+		List<City> adjCities = CollectionsFactory.createListFactory().createList();
+		Integer resp;
+		
+		System.out.println(player.getName()+" HAS USED --> Fast Move <-- ");
+		System.out.println("Select the city below where you want to move:");
+
+		 adjCities = player.getCity().maxMovement(5);
+
+		for (int i=0;i<adjCities.size();i++){
+			System.out.println("--> ["+i+"]"+ adjCities.get(i));
+		}
+
+		resp= GameFactory.takeParametersToIntegerTopValue("Option:", adjCities.size());
+		
+		player.moveCity(adjCities.get(resp));
 	}
 
 	@Override
 	public Integer getIdCard() {
-		return null;
+		return id;
 	}
 
 }
--- src/actionCards/TravelImpl.java
+++ src/actionCards/TravelImpl.java
@@ -1,23 +1,48 @@
 package actionCards;
 
+import game.factory.GameFactory;
+
+import java.util.List;
+
+import player.Player;
+
+import utiles.factoria.CollectionsFactory;
+import board.City;
 import card.CardImpl;
 
 public class TravelImpl extends CardImpl implements ActionCard {
-
-	public TravelImpl() {
+	private Integer id;
+	Player player;
+	
+	public TravelImpl(Player p) {
 		super("Travel", "You can move until 3 cities");
+		player=p;
+		id =1;
 	}
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
+		List<City> adjCities = CollectionsFactory.createListFactory().createList();
+		Integer resp;
+		
+		System.out.println(player.getName()+" HAS USED --> Fast Move <-- ");
+		System.out.println("Select the city below where you want to move:");
+
+		 adjCities = player.getCity().maxMovement(3);
+
+		for (int i=0;i<adjCities.size();i++){
+			System.out.println("--> ["+i+"]"+ adjCities.get(i));
+		}
+
+		resp= GameFactory.takeParametersToIntegerTopValue("Option:", adjCities.size());
+		
+		player.moveCity(adjCities.get(resp));
 		
 	}
 
 	@Override
 	public Integer getIdCard() {
-		// TODO Auto-generated method stub
-		return null;
+		return id;
 	}
 
 }
