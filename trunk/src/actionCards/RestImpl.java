e944fb3c3f925f058e651cb214dbaa3d00b60a18
marcetsii <marcetsii@ef17d506-aae0-06ea-4d23-8d1ec7ecb975>
2010-12-02 19:10:40 +0000

    Creada la carta de Acción RestImpl, que correspondía a la que habiamos llamado HoldImpl. Esta carta de acción no estaba creada, probablemente el inglés me jugó una mala pasada entre rest y hold.

---
+++ trunk/src/actionCards/RestImpl.java
@@ -0,0 +1,74 @@
+package actionCards;
+
+import java.util.List;
+
+import game.factory.GameFactory;
+import bag.TalentBag;
+import board.City;
+import player.Player;
+import talent.Acrobat;
+import talent.AcrobatImpl;
+import talent.Clown;
+import talent.ClownImpl;
+import talent.Talent;
+import utiles.factoria.CollectionsFactory;
+import card.CardImpl;
+
+public class RestImpl extends CardImpl implements ActionCard {
+	Integer id;
+	Player player;
+	TalentBag talentbag;
+	
+	public RestImpl(Player player, TalentBag talentbag) {
+		super("RestImpl","If you are in Canada (Winnipeg, Montreal or Toronto), " +
+				"you can take one clown, one acrobat or rise your reputation in one level" );
+		this.id = 7;
+		this.player = player;
+		this.talentbag = talentbag;
+	}
+
+	public void execute() {
+		City city = player.getCity();
+		
+		if(city.isCanada()){
+			String mess = "Do you want? \n 1: clown \n 2: acrobat \n 3: rise your reputation in one level";
+			String cond="1,2,3";
+			String resp= GameFactory.takeParametersToStringRestricted(mess, cond);
+			Integer respuesta = new Integer(resp);
+			List<Talent> listtalent = CollectionsFactory.createListFactory().createList();
+			Clown clown = new ClownImpl();
+			Acrobat acrobat = new AcrobatImpl();
+			
+			if(respuesta == 1){
+				if(talentbag.getNumTalents(clown)>0){
+					listtalent.add(talentbag.removeTalent(clown));
+					player.addTalent(listtalent);
+				}else{
+					System.out.println("Clowns are not available");
+					execute();
+				}
+			}
+			if(respuesta == 2){
+				if(talentbag.getNumTypeTalent(acrobat)>0){
+					listtalent.add(talentbag.removeTalent(acrobat));
+					player.addTalent(listtalent);
+				}else{
+					System.out.println("Acrobat are not available");
+					execute();
+				}
+				
+			}
+			if(respuesta == 3){
+				if(!player.addReputation(1)){
+					System.out.println("Rise reputation are not available");
+					execute();
+				}
+			}
+		}
+	}
+
+	public Integer getIdCard() {
+		return id;
+	}
+
+}
