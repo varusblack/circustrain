de08e1d596fd3badb83290df86af6042e053ee5e
jenkin90 <jenkin90@ef17d506-aae0-06ea-4d23-8d1ec7ecb975>
2010-12-08 18:18:43 +0000

    Cargar de fichero las bolsas de performance

--- src/bag/PerformanceBagImpl.java
+++ src/bag/PerformanceBagImpl.java
@@ -1,6 +1,9 @@
 package bag;
 
 import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -8,19 +11,85 @@ import java.util.Map;
 import performance.*;
 import talent.*;
 import utiles.factoria.CollectionsFactory;
+import utiles.factoria.ReadFile;
 
 public class PerformanceBagImpl implements PerformanceBag {
+	private List<Performance> greenBag = CollectionsFactory.createListFactory().createList();
+	private List<Performance> redBag = CollectionsFactory.createListFactory().createList();
+	private List<Performance> yellowBag= CollectionsFactory.createListFactory().createList();
+	
+	
 	public PerformanceBagImpl() {
 		super();
-		this.createGreenBag();
-		this.createYellowBag();
-		this.createRedBag();
-		
+//		this.createGreenBag();
+//		this.createYellowBag();
+//		this.createRedBag();
+	}
+	
+	public PerformanceBagImpl(String file){
+		this();
+		String fileLine=null;
+		BufferedReader fileReader = null;
+		try {
+			//Soluciona el problema cuando movemos el trabajo de un ordenador a otro.
+			fileReader = ReadFile.readTextFile(System.getProperty("user.dir")+file);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		try {
+			while ((fileLine = fileReader.readLine())!=null) {
+				   Color color=null;
+				   String []readedString=fileLine.split(",");
+				   if(readedString[1]=="g"){
+						   color=Color.green;
+				   }
+				   if(readedString[1]=="y"){
+					   color=Color.yellow;
+				   }
+				   if(readedString[1]=="r"){
+					   color=Color.red;
+				   }
+				   if(readedString[0].equals("D")){
+					   String description="Performance Demand";
+					   Integer basicPoints=new Integer(readedString[4]);
+					   Boolean twoWeeks=new Boolean(readedString[3]);
+					   Map<Talent,Integer> demandedTalents=CollectionsFactory.createMapFactory().createMap();
+					   if(!readedString[4].equals("0")){
+						   demandedTalents.put(new ClownImpl(), new Integer(readedString[4]));
+					   }
+					   if(!readedString[5].equals("0")){
+						   demandedTalents.put(new AcrobatImpl(), new Integer(readedString[5]));
+					   }
+					   if(!readedString[6].equals("0")){
+						   demandedTalents.put(new BigCatImpl(), new Integer(readedString[6]));
+					   }
+					   if(!readedString[7].equals("0")){
+						   demandedTalents.put(new ElephantImpl(), new Integer(readedString[7]));
+					   }
+					   if(!readedString[8].equals("0")){
+						   demandedTalents.put(new FreakShowImpl(), new Integer(readedString[8]));
+					   }
+					   if(!readedString[9].equals("0")){
+						   demandedTalents.put(new HorseImpl(), new Integer(readedString[9]));
+					   }
+					   if(!readedString[10].equals("0")){
+						   demandedTalents.put(new HumanCannonballImpl(), new Integer(readedString[10]));
+					   }
+					   Performance performanceToAdd=new PerformanceDemandImpl(color, description, basicPoints, demandedTalents, twoWeeks);
+					   addPerformance(performanceToAdd);					   
+				   }
+
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}   
 	}
 
-	private List<Performance> greenBag = CollectionsFactory.createListFactory().createList();;
-	private List<Performance> redBag = CollectionsFactory.createListFactory().createList();;
-	private List<Performance> yellowBag= CollectionsFactory.createListFactory().createList();;
+	
 	@Override
 
 	
@@ -54,6 +123,7 @@ public class PerformanceBagImpl implements PerformanceBag {
 	}
 
 	@Override
+
 	public List<Performance> createRedBag() {
 		// Aunque sea el mismo codigo que el anterior y el siguiente, realmente cambiara
 		List<Talent> listatalentos= CollectionsFactory.createListFactory().createList();
@@ -89,6 +159,7 @@ public class PerformanceBagImpl implements PerformanceBag {
 	}
 
 	@Override
+
 	public List<Performance> getGreenBag() {
 		// TODO Auto-generated method stub
 		return greenBag;
