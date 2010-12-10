a1a0930d84dbe5a8ac67dd17e6a4c61daee4c8d1
varusblack <varusblack@ef17d506-aae0-06ea-4d23-8d1ec7ecb975>
2010-12-10 09:16:47 +0000

    Arreglados comandos

---
+++ src/game/CommandToPay.java
@@ -0,0 +1,37 @@
+package game;
+
+import game.factory.GameFactory;
+
+import java.util.List;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import player.Player;
+import talent.Talent;
+import utiles.factoria.CollectionsFactory;
+
+public class CommandToPay extends AbstractCommand{
+	
+	private Player player;
+	private CircusTrainGame game;
+	
+	public CommandToPay (Player player,CircusTrainGame game){
+		this.player=player;
+		this.game=game;
+	}
+	
+	private Integer talentsNumber;
+	private Integer totalMoneyToPay;
+	private void showTalents(){
+	Set<Entry<Talent,Integer>> playerTalents=player.getTalents().entrySet();
+	List<Entry<Talent,Integer>> playerTalentList=CollectionsFactory.createListFactory().createList();
+	
+	for(Entry<Talent,Integer> entry:playerTalents){
+		
+	}
+	
+	public void execute(){
+			
+	}
+
+}
