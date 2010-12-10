3bd9fa66f5d10481fd8db13a8e48f43e7400f15d
jenkin90 <jenkin90@ef17d506-aae0-06ea-4d23-8d1ec7ecb975>
2010-12-10 09:10:29 +0000

    Empezando a integrar el juego. Creada la clase para leer de teclado.

--- trunk/src/game/factory/GameFactory.java
+++ trunk/src/game/factory/GameFactory.java
@@ -1,8 +1,8 @@
 package game.factory;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.List;
+import game.CircusTrainGame;
+import game.CircusTrainGameImpl;
+
 import java.util.Random;
 import bag.PerformanceBag;
 import bag.PerformanceBagImpl;
@@ -20,7 +20,6 @@ import talent.FreakShowImpl;
 import talent.HorseImpl;
 import talent.HumanCannonballImpl;
 import talent.Talent;
-import tipos.Cadenas;
 
 public class GameFactory {
 	
@@ -40,7 +39,7 @@ public class GameFactory {
 	
 	
 	public static PerformanceBag createPerformanceBag(){
-		return new PerformanceBagImpl();
+		return new PerformanceBagImpl("/data/performancecfg.txt");
 	}
 	
 	public static TalentBag createTalentBag(){
@@ -57,58 +56,10 @@ public class GameFactory {
 		return new BoardImpl("/data/boardcfg.txt");
 	}
 	
-	public static String takeParametersToString(String message){
-		String line=null;
-		try {
-			System.out.print(message.toString());
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			line = br.readLine();
-		} catch (Exception e) {
-		}
-		return line;
-	}
-	
-	public static Integer takeParametersToInteger(String message){
-		Integer number=-1;
-		try {
-			System.out.print(message.toString());
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			String linea = br.readLine();
-			number = Integer.parseInt(linea);
-		} catch (Exception e) {
-		}
-		return number;
-	}
-	
 	public static Integer throwDice(){
 		Random random = new Random();
 		int tiradaDado = (Math.abs(random.nextInt()) % 6) + 1;
 		return new Integer(tiradaDado);
-	}
-	
-	public static String takeParametersToStringRestricted(String message,String condition){
-		List<String> conditions=Cadenas.separaElementos(condition,",");
-		String line="";
-		while(!conditions.contains(line)){
-			line= GameFactory.takeParametersToString(message);
-		}
-		return line;		
-	}
-	
-	public static Integer takeParametersToIntegerRestricted(String message, String condition){
-		Integer number = -1;
-		List<String> conditions=Cadenas.separaElementos(condition,",");
-		while(!conditions.contains(number.toString())){
-			number= takeParametersToInteger(message);
-		}
-		return number;		
-	}
-	public static Integer takeParametersToIntegerTopValue(String message, Integer top){
-		Integer number = -1;
-		while(!(number >= 0 && number <= top)){
-			number= takeParametersToInteger(message);
-		}
-		return number;		
 	}
 	
 	public static Talent createTalent(String talentToCreate){
@@ -136,5 +87,8 @@ public class GameFactory {
 			return new HumanCannonballImpl();
 		}
 		return null;
+	}
+	public static CircusTrainGame createCircusTrainGame(){
+		return new CircusTrainGameImpl();
 	}
 }
