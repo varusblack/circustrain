05490dac8c5251283e6efd0e042544336eee5f67
fragelbreak <fragelbreak@ef17d506-aae0-06ea-4d23-8d1ec7ecb975>
2010-12-18 19:20:34 +0000

    circo mapaa redim

--- trunk/src/game/factory/ShowInfo.java
+++ trunk/src/game/factory/ShowInfo.java
@@ -4,28 +4,30 @@ import game.CircusTrainGame;
 import player.Player;
 
 public class ShowInfo {
-	
+
 	CircusTrainGame ctg;
 	Player p;
-	
-	public ShowInfo(CircusTrainGame ctg, Player p){
-		this.ctg=ctg;
-		this.p=p;
+
+	public ShowInfo(CircusTrainGame ctg, Player p) {
+		this.ctg = ctg;
+		this.p = p;
 	}
 
 	public void refresh() {
-		
+
 		/*
 		 * Mostrar info de estado del tablero:
 		 */
 		System.out.println("Estado del tablero: \n\n");
 
-		System.out.println("Las ciudades sin actuaciones son: \n" + ctg.getBoard().getCitiesWithoutPerformance().toString());
+		System.out.println("Las ciudades sin actuaciones son: \n"
+				+ ctg.getBoard().getCitiesWithoutPerformance().toString());
 		System.out.println("======================================");
-		System.out.println("Las ciudades con actuaciones son: \n" + ctg.getBoard().getCitiesWithPerfomance());
+		System.out.println("Las ciudades con actuaciones son: \n"
+				+ ctg.getBoard().getCitiesWithPerfomance());
 		System.out.println("======================================");
 		System.out.println("======================================");
-		
+
 		/*
 		 * Mostrar info de estado del jugador:
 		 */
@@ -36,23 +38,17 @@ public class ShowInfo {
 		System.out.println("Salario actual: \n" + p.getMoney());
 		System.out.println("Actuación máxima: \n" + p.getPerformanceMax());
 		System.out.println("Reputación: \n" + p.getReputation());
-		System.out.println("Puntuación más alta del dado: \n" + p.getHigherDiceScore());
+		System.out.println("Puntuación más alta del dado: \n"
+				+ p.getHigherDiceScore());
 		System.out.println("Puntos de Victoria: \n" + p.getVictoryPoints());
-		System.out.println("Las cartas de acción son: \n" + p.getActionCards().toString());
-		System.out.println("Las cartas de acción son: \n" + p.getActionCards().toString());
-
-		System.out.println("Las cartas de acción son: \n" + p.getActionCards().toString());
+		System.out.println("Semanas restantes para actuación: \n"
+				+ p.getWeeksToPerformance());
+		System.out.println("Talentos: \n" + p.getTalents());
+		System.out.println("Cartas de acción: \n" + p.getActionCards());
 		System.out.println("\n======================================\n");
-		System.out.println("Las cartas de acción son: \n" + p.);
-
-
-		
-		
-		
-		
+		System.out.println("Cartas de acción usadas: \n"
+				+ p.getPerfomancesUsed());
 
 	}
-	
-	
 
 }
