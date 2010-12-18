08e030a47e504921c779967486bfa842c47816f2
jenkin90 <jenkin90@ef17d506-aae0-06ea-4d23-8d1ec7ecb975>
2010-12-18 16:30:34 +0000

    abstrayendo

--- trunk/src/game/CircusTrainGame.java
+++ trunk/src/game/CircusTrainGame.java
@@ -5,13 +5,19 @@ import game.factory.GameFactory;
 import java.util.List;
 import java.util.Map;
 
+import commands.CommandExecuteCase;
+import commands.CommandPlayerState;
+import commands.CommandSelectCase;
+
 import bag.PerformanceBag;
 import bag.TalentBag;
 import board.Board;
+import board.City;
 import performance.Performance;
 import player.Player;
 import talent.Talent;
 import utiles.factoria.CollectionsFactory;
+import utiles.factoria.readDataFromKeyBoard;
 
 public abstract class CircusTrainGame{
 	protected static String month;
@@ -30,6 +36,8 @@ public abstract class CircusTrainGame{
 		performanceBag= GameFactory.createPerformanceBag();
 		talentBag = GameFactory.createTalentBag();
 		theClown.add(clown);
+		refreshMonth();		
+		completeBoardPerformances();
 	}	
 	
 	public void refreshMonth(){
@@ -79,4 +87,93 @@ public abstract class CircusTrainGame{
 	public String getFollowingAction(){
 		return followingAction;
 	}
+	
+	protected City selectCanadianCity(){
+		List<City> canadianCityList=board.getCanadianCities();
+		String askCanadianCity="Seleccione una ciudad canadiense: "+"\n";
+		String askCanadianCityConditions="";
+		
+		for(int i=0;i<canadianCityList.size();i++){
+			Integer optionsCount=i+1;
+			askCanadianCityConditions=askCanadianCityConditions+(optionsCount)+",";
+			askCanadianCity=askCanadianCity+(optionsCount)+" : "+canadianCityList.get(i).getName()+"\n";
+		}
+		String selectedCanadianCity=readDataFromKeyBoard.takeParametersToStringRestricted(askCanadianCity, askCanadianCityConditions);
+		Integer citySelector=new Integer(selectedCanadianCity);
+		return canadianCityList.get(citySelector);
+	}
+	
+	public void runGame(){
+		
+		System.out.println("\n \n Ciudades con actuaciones: "+ board.getCitiesWithPerfomance() +"\n \n");
+		
+		for(Player playerSelectsCity:playerList){
+			playerSelectsCity.moveCity(selectCanadianCity());
+		}
+		
+		while(week<27){
+			String oldMonth=this.getMonth();
+			refreshMonth();
+			String newMonth=this.getMonth();
+			//Si hay cambio de mes se llevaran a cabo las acciones de fin de mes
+			if(!(oldMonth.equals(newMonth))){
+				finalMonth();
+				rotatePlayers();
+			}
+			//Mostrar ciudades con eventos
+			System.out.println(board.getCitiesWithPerfomance().toString());
+			System.out.println("\n \n This is the Week " + week + " and Mounth " + month);
+			
+			for(Player currentPlayer : playerList){
+				//Se muestra el estado del jugador
+				System.out.println("\n \n Its you turn, " + currentPlayer.getName());
+				CommandPlayerState playerState = new CommandPlayerState(currentPlayer);				
+				playerState.execute();
+				
+				//Se le pregunta al jugador que va a hacer segun sus condiciones actuales
+				CommandSelectCase selectCase = new CommandSelectCase(currentPlayer, this);
+				selectCase.execute();
+				
+				//Se lleva a cabo la accion que el jugador a elegido
+				CommandExecuteCase executeCase=new CommandExecuteCase(currentPlayer, this);
+				executeCase.execute();				
+			}
+			//Se mira el número de ciudades y se añaden si es el caso
+			completeBoardPerformances();					
+			week++;			
+		}
+		this.gameOver();
+	}
+	
+	public TalentBag getTalentBag(){
+		return talentBag;
+	}
+	
+	public List<Player> getPlayerList(){
+		return playerList;
+	}	
+	
+	public String getMonth(){
+		return month;
+	}	
+	
+	public void setFollowingAction(String action){
+		followingAction=action;
+	}
+	
+	protected void rotatePlayers(){
+		if(numberOfPlayers!=1){
+			List<Player> newPlayerList=CollectionsFactory.createListFactory().createList();
+			for(int i=1;i<playerList.size();i++){
+				newPlayerList.add(playerList.get(i));	
+			}
+			newPlayerList.add(playerList.get(0));
+			playerList.clear();
+			playerList.addAll(newPlayerList);
+		}
+		
+		
+		
+	}
+
 }
