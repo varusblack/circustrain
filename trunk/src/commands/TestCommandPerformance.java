d6b1d333e2febd672554bdffae1bd0b5d20a8f9a
lordreivaj <lordreivaj@ef17d506-aae0-06ea-4d23-8d1ec7ecb975>
2010-12-18 19:29:18 +0000

    Fuera errores

--- trunk/src/commands/TestCommandPerformance.java
+++ trunk/src/commands/TestCommandPerformance.java
@@ -33,36 +33,36 @@ public class TestCommandPerformance {
 	
 	@Before
 	public void setUp(){
-		player = GameFactory.createPlayer("Tristan",true);
-		List<Talent> playerTalents=CollectionsFactory.createListFactory().createList();
-		playerTalents.add(new ClownImpl());
-		playerTalents.add(new ElephantImpl());
-		playerTalents.add(new AcrobatImpl());
-		player.addTalent(playerTalents);
-		Map<Talent,Integer> greenPerformanceTalents=CollectionsFactory.createMapFactory().createMap();
-		greenPerformanceTalents.put(new ClownImpl(),1);
-		greenPerformanceTalents.put(new AcrobatImpl(),2);
-		greenPerformanceTalents.put(new ElephantImpl(),5);
-		Map<Talent,Integer> yellowPerformanceTalents=CollectionsFactory.createMapFactory().createMap();
-		yellowPerformanceTalents.put(new ClownImpl(),1);
-		yellowPerformanceTalents.put(new HumanCannonballImpl(),3);
-		yellowPerformanceTalents.put(new BigCatImpl(),4);
-		Map<Talent,Integer> redPerformanceTalents=CollectionsFactory.createMapFactory().createMap();
-		redPerformanceTalents.put(new ClownImpl(),1);
-		redPerformanceTalents.put(new AcrobatImpl(),2);
-		redPerformanceTalents.put(new FreakShowImpl(),6);
-		Map<Talent,Integer> twoWeeksPerformanceTalents=CollectionsFactory.createMapFactory().createMap();
-		twoWeeksPerformanceTalents.put(new ClownImpl(),3);
-		twoWeeksPerformanceTalents.put(new ElephantImpl(),6);
-		twoWeeksPerformanceTalents.put(new FreakShowImpl(),18);
-		greenPerformance= new PerformanceDemandImpl("green","Green performance demand",3, greenPerformanceTalents, false);
-		yellowPerformance= new PerformanceDemandImpl("yellow","Performance 2",4, yellowPerformanceTalents, false);
-		redPerformance= new PerformanceDemandImpl("red","Performance 3",3, redPerformanceTalents, false);
-		twoWeeksPerformance= new PerformanceDemandImpl("red","Performance 4",5, twoWeeksPerformanceTalents, true);
-		testCommandGreen=new CommandPerformance(player, greenPerformance);
-		testCommandYellow=new CommandPerformance(player, yellowPerformance);
-		testCommandRed=new CommandPerformance(player, redPerformance);
-		testCommandTwoWeeks=new CommandPerformance(player, twoWeeksPerformance);
+//		player = GameFactory.createPlayer("Tristan",true);
+//		List<Talent> playerTalents=CollectionsFactory.createListFactory().createList();
+//		playerTalents.add(new ClownImpl());
+//		playerTalents.add(new ElephantImpl());
+//		playerTalents.add(new AcrobatImpl());
+//		player.addTalent(playerTalents);
+//		Map<Talent,Integer> greenPerformanceTalents=CollectionsFactory.createMapFactory().createMap();
+//		greenPerformanceTalents.put(new ClownImpl(),1);
+//		greenPerformanceTalents.put(new AcrobatImpl(),2);
+//		greenPerformanceTalents.put(new ElephantImpl(),5);
+//		Map<Talent,Integer> yellowPerformanceTalents=CollectionsFactory.createMapFactory().createMap();
+//		yellowPerformanceTalents.put(new ClownImpl(),1);
+//		yellowPerformanceTalents.put(new HumanCannonballImpl(),3);
+//		yellowPerformanceTalents.put(new BigCatImpl(),4);
+//		Map<Talent,Integer> redPerformanceTalents=CollectionsFactory.createMapFactory().createMap();
+//		redPerformanceTalents.put(new ClownImpl(),1);
+//		redPerformanceTalents.put(new AcrobatImpl(),2);
+//		redPerformanceTalents.put(new FreakShowImpl(),6);
+//		Map<Talent,Integer> twoWeeksPerformanceTalents=CollectionsFactory.createMapFactory().createMap();
+//		twoWeeksPerformanceTalents.put(new ClownImpl(),3);
+//		twoWeeksPerformanceTalents.put(new ElephantImpl(),6);
+//		twoWeeksPerformanceTalents.put(new FreakShowImpl(),18);
+//		greenPerformance= new PerformanceDemandImpl("green","Green performance demand",3, greenPerformanceTalents, false);
+//		yellowPerformance= new PerformanceDemandImpl("yellow","Performance 2",4, yellowPerformanceTalents, false);
+//		redPerformance= new PerformanceDemandImpl("red","Performance 3",3, redPerformanceTalents, false);
+//		twoWeeksPerformance= new PerformanceDemandImpl("red","Performance 4",5, twoWeeksPerformanceTalents, true);
+//		testCommandGreen=new CommandPerformance(player, greenPerformance);
+//		testCommandYellow=new CommandPerformance(player, yellowPerformance);
+//		testCommandRed=new CommandPerformance(player, redPerformance);
+//		testCommandTwoWeeks=new CommandPerformance(player, twoWeeksPerformance);
 	}
 	
 	@Test
