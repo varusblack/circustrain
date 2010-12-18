11e88dfa49ec6e31f8906a01c39397363c922827
jenkin90 <jenkin90@ef17d506-aae0-06ea-4d23-8d1ec7ecb975>
2010-12-18 13:04:13 +0000

    Y yo que se, me lo manda Tristan

--- trunk/src/game/CircusTrainGameImpl.java
+++ trunk/src/game/CircusTrainGameImpl.java
@@ -1,44 +1,32 @@
+                                                                     
+                                                                     
+                                                                     
+                                             
 package game;
 
 import game.factory.GameFactory;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.Map.Entry;
 
-import commands.CommandExecuteCase;
-import commands.CommandPay;
-import commands.CommandPlayerState;
-import commands.CommandSelectCanadianCity;
-import commands.CommandSelectCase;
-import commands.CommandStealTalent;
-
-import actionCards.ActionCard;
 import bag.PerformanceBag;
 import bag.TalentBag;
 import board.Board;
 import performance.Performance;
 import player.Player;
-import talent.Clown;
 import talent.Talent;
 import utiles.factoria.CollectionsFactory;
-import utiles.factoria.readDataFromKeyBoard;
 
-public class CircusTrainGameImpl implements CircusTrainGame{
-	
-	protected List<Player> playerList;
-	protected static Integer week=0;
-	protected static String month="APRIL";
+public abstract class CircusTrainGameImpl implements CircusTrainGame{
+	protected static String month;
+	protected static Integer week;
 	protected Board board;
 	protected PerformanceBag performanceBag;
-	protected TalentBag talentBag;
-	protected Integer numberOfPlayers;
-	protected String followingAction;	
-	protected Boolean advancedMode;
+	protected String followingAction;
 	protected List<Talent> theClown=CollectionsFactory.createListFactory().createList();
 	protected Talent clown = GameFactory.createTalent("CLOWN");
-	
+	protected TalentBag talentBag;
+	protected List<Player> playerList;
 	
 	public CircusTrainGameImpl(){
 		playerList=CollectionsFactory.createListFactory().createList();
@@ -46,150 +34,19 @@ public class CircusTrainGameImpl implements CircusTrainGame{
 		performanceBag= GameFactory.createPerformanceBag();
 		talentBag = GameFactory.createTalentBag();
 		theClown.add(clown);
-	}
-	
-	public void runGame() {
-		refreshMonth();
-		
-		completeBoardPerformances();
-		System.out.println("\n \n Cities with performance: "+ board.getCitiesWithPerfomance() +"\n \n");
-		
-		for(Player playerSelectsCity:playerList){
-			CommandSelectCanadianCity selectCanadianCity=new CommandSelectCanadianCity(playerSelectsCity, this);
-			selectCanadianCity.execute();
-		}
-		
-		while(week<27){
-			String oldMonth=this.getMonth();
-			refreshMonth();
-			String newMonth=this.getMonth();
-			
-			
-			//Si hay cambio de mes se llevaran a cabo las acciones de fin de mes
-			if(!(oldMonth == newMonth)){
-				finalMonth();
-				rotatePlayers();
-			}
-			System.out.println(board.getCitiesWithPerfomance().toString());
-			System.out.println("\n \n This is the Week " + week + " and Mounth " + month);
-			
-			for(Player currentPlayer : playerList){
-								
-				System.out.println("\n \n Its you turn, " + currentPlayer.getName());
-				CommandPlayerState playerState = new CommandPlayerState(currentPlayer);				
-				playerState.execute();
-				
-				//Se le pregunta al jugador que va a hacer segun sus condiciones actuales
-				CommandSelectCase selectCase = new CommandSelectCase(currentPlayer, this);
-				selectCase.execute();
-				
-				//Se lleva a cabo la accion que el jugador a elegido
-				CommandExecuteCase executeCase=new CommandExecuteCase(currentPlayer, this);
-				executeCase.execute();				
-			}
-			completeBoardPerformances();					
-			week++;			
-		}
-		gameOver();
-	}
-	
-	public void finalMonth(){
-		//Puntos de victoria segun el nº de talentos
-		compareTalentsCountAndAddVictoryPoints();
-		//Puntos de victoria segun actuacion maxima
-		int comparatorPerformancePoints =playerList.get(0).getPerformanceMax().compareTo(playerList.get(1).getPerformanceMax());
-		if(comparatorPerformancePoints>0)
-			playerList.get(0).addVictoryPoints(4);
-		if(comparatorPerformancePoints<0)
-			playerList.get(1).addVictoryPoints(4);			
-		//Robar talentos
-		//En modo basico solo puede robar el que tenga menos puntos de victoria
-
-	}
-	
-	//Game over de dos players.
-	public void gameOver() {
-		wageCardNoDiscarded();
-		higherClownNumber();
-		higherMoneyAmount();
-		higherPerformancesNumber();
-	//TODO SOLO PARA AdvancedTwoPlayersGame	
-		//higherReputation();
-		noClownsNoAnimals();
-		results();
-	}
-
-	
-
-
-	public TalentBag getTalentBag(){
-		return talentBag;
-	}
-	
-	public List<Player> getPlayerList(){
-		return playerList;
 	}	
 	
-	public String getMonth(){
-		return month;
-	}	
-	
-	public void setFollowingAction(String action){
-		followingAction=action;
+	public void refreshMonth(){
+			if(week>=0 && week<=3)month = "APRIL";
+			if(week>=4 && week<=8) month = "MAY";
+			if(week>=9 && week<=12)month = "JUNE";
+			if(week>=13 && week<=17)month = "JULY";
+			if(week>=18 && week<=21)month = "AUGUST";
+			if(week>=22 && week<=26)month = "SEPTEMBER";	
+		
 	}
 	
-	public String getFollowingAction() {
-		return followingAction;
-	}
-	
-	public Board getBoard(){
-		return board;
-	}
-	
-	//====================================== X =====================================
-	
-	protected void refreshMonth(){
-		if(week>=0 && week<=3)month = "APRIL";
-		if(week>=4 && week<=8) month = "MAY";
-		if(week>=9 && week<=12)month = "JUNE";
-		if(week>=13 && week<=17)month = "JULY";
-		if(week>=18 && week<=21)month = "AUGUST";
-		if(week>=22 && week<=26)month = "SEPTEMBER";	
-	}
-	
-	protected void rotatePlayers(){
-		if(numberOfPlayers!=1){
-			List<Player> newPlayerList=CollectionsFactory.createListFactory().createList();
-			for(int i=1;i<playerList.size();i++){
-				newPlayerList.add(playerList.get(i));	
-			}
-			newPlayerList.add(playerList.get(0));
-			playerList.clear();
-			playerList.addAll(newPlayerList);
-		}
-	}
-	private void compareTalentsCountAndAddVictoryPoints(){
-		List<Map<Talent,Integer>> playersTalentsList=CollectionsFactory.createListFactory().createList();
-		for(Player player:playerList){
-			playersTalentsList.add(player.getTalents());
-		}
-		//TODO List<Player>
-		Set<Entry<Talent,Integer>> numberOfTalents1=playersTalentsList.get(0).entrySet();			
-		Set<Entry<Talent,Integer>> numberOfTalents2=playersTalentsList.get(1).entrySet();		
-		for(Entry<Talent,Integer> entry1:numberOfTalents1){
-			for(Entry<Talent,Integer> entry2:numberOfTalents2){
-				if(entry1.getKey().equals(entry2.getKey())){
-					int compare=entry1.getValue().compareTo(entry2.getValue());
-					if(compare>0)
-						playerList.get(0).addVictoryPoints(entry1.getValue());
-					if(compare<0)
-						playerList.get(1).addVictoryPoints(entry2.getValue());						
-					break;
-				}
-			}
-		}
-	}
-	protected void completeBoardPerformances(){
+	public void completeBoardPerformances(){
 		if(month.equals("APRIL") || month.equals("MAY")){
 			while(board.getCitiesWithPerfomance().size()<8){
 				Performance randomPerformance = performanceBag.getPerformance("green");
@@ -213,56 +70,9 @@ public class CircusTrainGameImpl implements CircusTrainGame{
 		}
 	}
 	
-	protected void wageCardNoDiscarded() {
-		for(Player player: playerList){
-			for(ActionCard actioncard : player.getActionCards()){
-				if(actioncard.getIdCard() == 5){
-					player.addVictoryPoints(-3);
-					CommandPay cmp = new CommandPay(player, this, 2);
-					cmp.execute();
-					break;
-				}
-			}
-		}
-		
-	}
-	protected void higherClownNumber(){
-		Integer playerClownNumber=0;
-		Integer maximumClownNumber=-1;
-		Integer sameClownNumberTimes=0;
-		Integer previousClownNumber=0;
-		List<Integer> clownNumber=CollectionsFactory.createListFactory().createList();
-		for(int i=0;i<playerList.size();i++){
-			Player thisPlayer=playerList.get(i);
-			Map<Talent,Integer> thisPlayerTalents = thisPlayer.getTalents();
-			if(thisPlayerTalents.containsKey(Clown.class)){
-				playerClownNumber=thisPlayer.getTalents().get(Clown.class);			
-			}else{
-				playerClownNumber=0;
-			}
-			clownNumber.add(playerClownNumber);
-			if(maximumClownNumber<playerClownNumber){
-				maximumClownNumber=playerClownNumber;
-			}
-			if(i>0){
-				Player previousPlayer=playerList.get(i-1);
-				if(previousPlayer.getTalents().containsKey(Clown.class)){
-					previousClownNumber=thisPlayer.getTalents().get(Clown.class);			
-				}else{
-					previousClownNumber=0;
-				}
-			}
-			if(maximumClownNumber==previousClownNumber){
-				sameClownNumberTimes++;
-			}
-		}
-		if(sameClownNumberTimes==0){
-			Integer playerIndex= clownNumber.indexOf(maximumClownNumber);
-			playerList.get(playerIndex).addVictoryPoints(3);
-		}
-	}
+	public abstract void gameOver();
 	
-	protected void noClownsNoAnimals(){
+	public void noClownsNoAnimals(){
 		for(Player thisPlayer:playerList){
 			Map<Talent,Integer> thisPlayerTalents = thisPlayer.getTalents();
 			if(!thisPlayerTalents.containsKey(talent.Clown.class)){
@@ -276,126 +86,4 @@ public class CircusTrainGameImpl implements CircusTrainGame{
 			}			
 		}
 	}
-	
-	protected void higherMoneyAmount(){
-		List<Integer> playersMoney=CollectionsFactory.createListFactory().createList();
-		Integer sameMoneyTimes=0;
-		Integer maximumMoney=-1;
-		Integer previousMoney=0;
-		for(int i=0;i<playerList.size();i++){
-			if(i>0){
-				previousMoney=playerList.get(i-1).getMoney();
-			}
-			Player thisPlayer=playerList.get(i);
-			playersMoney.add(thisPlayer.getMoney());
-			if(maximumMoney<thisPlayer.getMoney()){
-				maximumMoney=thisPlayer.getMoney();
-			}
-			if(previousMoney==thisPlayer.getMoney()){
-				sameMoneyTimes++;
-			}
-		}
-		if(sameMoneyTimes==0){
-			Integer playerIndex=playersMoney.indexOf(maximumMoney);
-			playerList.get(playerIndex).addVictoryPoints(3);
-		}
-	}
-	
-	protected void higherPerformancesNumber(){
-		List<Integer> playersNumberOfPerformances=CollectionsFactory.createListFactory().createList();
-		Integer sameNumberOfPerformancesTimes=0;
-		Integer maximumNumberOfPerformances=-1;
-		Integer previousNumberOfPerformances=0;
-		for(int i=0;i<playerList.size();i++){
-			if(i>0){
-				previousNumberOfPerformances=playerList.get(i-1).getPerfomancesUsed().size();
-			}
-			Player thisPlayer=playerList.get(i);
-			playersNumberOfPerformances.add(thisPlayer.getPerfomancesUsed().size());
-			if(maximumNumberOfPerformances<thisPlayer.getPerfomancesUsed().size()){
-				maximumNumberOfPerformances=thisPlayer.getPerfomancesUsed().size();
-			}
-			if(previousNumberOfPerformances==thisPlayer.getPerfomancesUsed().size()){
-				sameNumberOfPerformancesTimes++;
-			}
-		}
-		if(sameNumberOfPerformancesTimes==0){
-			Integer playerIndex=playersNumberOfPerformances.indexOf(maximumNumberOfPerformances);
-			playerList.get(playerIndex).addVictoryPoints(3);
-		}
-	}
-	
-//	protected void higherReputation(){
-//		List<Integer> playersReputation=CollectionsFactory.createListFactory().createList();
-//		Integer sameReputationTimes=0;
-//		Integer maximumReputation=-1;
-//		Integer previousReputation=0;
-//		for(int i=0;i<playerList.size();i++){
-//			if(i>0){
-//				previousReputation=playerList.get(i-1).getReputation();
-//			}
-//			Player thisPlayer=playerList.get(i);
-//			playersReputation.add(thisPlayer.getReputation());
-//			if(maximumReputation<thisPlayer.getReputation()){
-//				maximumReputation=thisPlayer.getReputation();
-//			}
-//			if(previousReputation==thisPlayer.getReputation()){
-//				sameReputationTimes++;
-//			}
-//		}
-//		if(sameReputationTimes==0){
-//			Integer playerIndex=playersReputation.indexOf(maximumReputation);
-//			playerList.get(playerIndex).addVictoryPoints(3);
-//		}
-//	}
-	protected void results(){
-		// TODO DOCUMENTAR CAMBIO
-		String winner="";
-		Integer maxVictoryPoints=0;
-		Integer maxPerformancePoints=0;
-		Integer victoryPointsDrawGame=0;
-		Integer performancePointsDrawGame=0;
-		Integer previousPlayerVictoryPoints=0;
-		Integer previousPlayerPerformancePoints=0;
-		List<Integer> playerVictoryPoints=CollectionsFactory.createListFactory().createList();
-		List<Integer> playerPerformancePoints=CollectionsFactory.createListFactory().createList();
-		for(int i=0;i<playerList.size();i++){
-			Player player=playerList.get(i);
-			if(i>0){
-				previousPlayerVictoryPoints=playerList.get(i-1).getVictoryPoints();
-				previousPlayerPerformancePoints=playerList.get(i-1).getPerformanceMax();
-			}
-			playerVictoryPoints.add(player.getVictoryPoints());
-			playerPerformancePoints.add(player.getPerformanceMax());
-			if(maxVictoryPoints<player.getVictoryPoints()){
-				maxVictoryPoints=player.getVictoryPoints();				
-			}
-			if(maxPerformancePoints<player.getPerformanceMax()){
-				maxPerformancePoints=player.getPerformanceMax();
-			}
-			if(player.getVictoryPoints()==previousPlayerVictoryPoints){
-				victoryPointsDrawGame=1;
-			}
-			if(player.getPerformanceMax()==previousPlayerPerformancePoints){
-				performancePointsDrawGame=1;
-			}
-			
-		}
-		
-		if(victoryPointsDrawGame==1){
-			if(performancePointsDrawGame==1){
-				winner="There's no winner."+"\n"+"============= DRAW GAME =============";
-			}else{
-				Integer winnerIndex=playerPerformancePoints.indexOf(maxPerformancePoints);
-				Player winnerPlayer=playerList.get(winnerIndex);
-				winner="Here's your winner! The winner is"+"\n"+"============= "+winnerPlayer.getName()+" =============";
-			}
-		}else{
-			Integer winnerIndex=playerVictoryPoints.indexOf(maxVictoryPoints);
-			Player winnerPlayer=playerList.get(winnerIndex);
-			winner="Here's your winner! The winner is"+"\n"+"============= "+winnerPlayer.getName()+" =============";
-		}
-		System.out.println(winner+"\n"+"\n"+"---=== Thank you for playing Train Circus! ===---");
-	}
-	
 }
