31228b82bd1632f6ddf1d6518407ce2f0f186e9e
marcetsii <marcetsii@ef17d506-aae0-06ea-4d23-8d1ec7ecb975>
2011-01-07 11:30:46 +0000

    Arrglado un bug de OverNighter

--- trunk/src/actionCards/OvernighterImpl.java
+++ trunk/src/actionCards/OvernighterImpl.java
@@ -35,7 +35,7 @@ public class OvernighterImpl extends ActionCardImpl implements ActionCard {
 			String question="¿Que quieres hacer?: Viajar[1],Viajar Primero y luego Actuar/Contratar[2] o Actuar/Contratar[3]";
 			answer = readDataFromKeyBoard.takeParametersToIntegerRestricted(question+"Opcion:","1,2,3");
 		}
-		if (getPlayer().getCity().hasPerfomance() && adjCitiesWithPerformance.size()==50){
+		if (getPlayer().getCity().hasPerfomance() && adjCitiesWithPerformance.size()==0){
 			String restriction = "La ciudad actual tiene performance, pero no hay ciudades con Performance cercanas.";
 			String question ="¿Que quieres hacer?: Viajar[1] o Actuar/Contratar[3]";
 			answer = readDataFromKeyBoard.takeParametersToIntegerRestricted(restriction+question+"Opcion:","1,3");
@@ -58,7 +58,6 @@ public class OvernighterImpl extends ActionCardImpl implements ActionCard {
 		
 		if (answer == 2){
 			System.out.println("Estas son las ciudades que poseen Performance.");
-			//super.movePlayer(player,2,true);
 
 			for (int i=0; i< adjCitiesWithPerformance.size() ; i++){
 				System.out.println("--> ["+i+"]"+ adjCitiesWithPerformance.get(i));
